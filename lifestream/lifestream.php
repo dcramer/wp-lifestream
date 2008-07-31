7a570fe74550615a774650feb4f459a8c9adfb1d
David Cramer <dcramer@gmail.com = David Cramer dcramer@gmail.com>
2008-07-31 15:25:24 +0000

    syntax fix

--- lifestream/lifestream.php
+++ lifestream/lifestream.php
@@ -269,7 +269,7 @@ function LifeStream($results=50)
     $hour_format = get_option('lifestream_hour_format');
     $day_format = get_option('lifestream_day_format');
 
-    $results = $wpdb->get_results(sprintf("SELECT * FROM `".LIFESTREAM_TABLE_PREFIX."data` WHERE `timestamp` > UNIX_TIMESTAMP(DATE_SUB(NOW(), INTERVAL 1 MONTH)) AND `visible` = 1 ORDER BY `timestamp` DESC LIMIT 0, '%s'", $wpdb->escape($results));
+    $results = $wpdb->get_results(sprintf("SELECT * FROM `".LIFESTREAM_TABLE_PREFIX."data` WHERE `timestamp` > UNIX_TIMESTAMP(DATE_SUB(NOW(), INTERVAL 1 MONTH)) AND `visible` = 1 ORDER BY `timestamp` DESC LIMIT 0, '%s'", $wpdb->escape($results)));
     
     include('pages/lifestream.inc');
 }
