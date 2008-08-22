38ef1377dc2cd2c46633aa17afdd5c313d6ff37d
zeeg <zeeg@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-08-22 00:21:46 +0000

    updated lightbox link
    git-svn-id: http://plugins.svn.wordpress.org/lifestream/trunk@60972 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- feeds.inc.php
+++ feeds.inc.php
@@ -276,7 +276,7 @@ class LifeStream_FlickrFeed extends LifeStream_Feed
     {        
         return array(
             'user_id' => array('User ID:', true, '', ''),
-            'enable_lightbox' => array('Enable Lightbox support on Flickr images.', false, true, false, 'Requires the <a href="http://www.viper007bond.com/wordpress-plugins/jquery-lightbox-for-native-galleries/">jQuery Lightbox plugin</a>.'),
+            'enable_lightbox' => array('Enable Lightbox support on Flickr images.', false, true, false, 'Requires the <a href="http://wordpress.org/extend/plugins/jquery-lightbox-for-native-galleries/">jQuery Lightbox plugin</a>.'),
         );
     }
 
