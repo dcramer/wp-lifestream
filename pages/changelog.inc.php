6ab2ada80832ca2b0a8de9c6ec347e0eb3f85683
David Cramer <dcramer@gmail.com = David Cramer dcramer@gmail.com>
2008-12-22 20:01:34 +0000

    Updated last.fm icon. Changelog is now pulled from SVN. Deleting a feed will now raise SQL errors as exceptions.

--- pages/changelog.inc.php
+++ pages/changelog.inc.php
@@ -1,5 +1,7 @@
 <h2><?php _e('LifeStream Change Log', 'lifestream'); ?></h2>
 
+<p>This is the current change log, pulled directly from the latest version. It allows you to see past, and future changes.</p>
+
 <pre>
-<?php readfile(dirname(LIFESTREAM_PLUGIN_FILE) . '/CHANGES'); ?>
+<?php echo htmlspecialchars(lifestream_file_get_contents('http://svn.wp-plugins.org/lifestream/trunk/CHANGES')); ?>
 </pre>
