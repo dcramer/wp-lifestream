66a90f38bae468c07bdc876365b3910efa7f5002
zeeg <zeeg@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-12-30 19:57:59 +0000

    Added a lifestream_feed_<extension_name> class to event wrappers. Added a link to the image icons in the lifestream-table display.
    git-svn-id: http://plugins.svn.wordpress.org/lifestream/trunk@83177 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- pages/lifestream-table.inc.php
+++ pages/lifestream-table.inc.php
@@ -25,9 +25,9 @@ if (count($events))
             $day = $this_day;
         }
         ?>
-        <tr class="lifestream_feedid_<?php echo $result->feed->get_constant('ID'); ?>">
+        <tr class="lifestream_feedid_<?php echo $result->feed->get_constant('ID'); ?> lifestream_feed_<?php echo $result->feed->get_constant('ID'); ?>">
             <td class="lifestream_icon">
-                <img src="<?php echo $result->feed->get_icon_url(); ?>" alt="<?php echo $result->feed->get_constant('ID'); ?> (feed #<?php echo $result->feed->id; ?>)" />
+                <a href="<?php echo htmlspecialchars($result->get_url()); ?>"><img src="<?php echo $result->feed->get_icon_url(); ?>" alt="<?php echo $result->feed->get_constant('ID'); ?> (feed #<?php echo $result->feed->id; ?>)" />
             </td>
             <td class="lifestream_hour">
                 <abbr title="<?php echo date("c", $timestamp); ?>"><?php echo date($hour_format, $timestamp); ?></abbr>
