d7e44e2d0fc88fdb429b7ee7dd7a5c8dd74002fb
David Cramer <dcramer@gmail.com = David Cramer dcramer@gmail.com>
2009-02-26 06:22:22 +0000

    Fix for curl not handling redirects

--- lifestream.php
+++ lifestream.php
@@ -103,24 +103,31 @@ function lifestream_file_get_contents($url)
     $handler = get_option('lifestream_url_handler');
     if (($handler == 'auto' && function_exists('curl_init')) || $handler == 'curl')
     {
-        $ch = curl_init();
-        $timeout = 5;
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+        $options = array( 
+            CURLOPT_RETURNTRANSFER => true,     // return web page 
+            CURLOPT_HEADER         => false,    // don't return headers 
+            CURLOPT_FOLLOWLOCATION => true,     // follow redirects 
+            CURLOPT_ENCODING       => "",       // handle all encodings 
+            //CURLOPT_USERAGENT      => "spider", // who am i 
+            CURLOPT_AUTOREFERER    => true,     // set referer on redirect 
+            CURLOPT_CONNECTTIMEOUT => 10,      // timeout on connect 
+            CURLOPT_TIMEOUT        => 10,      // timeout on response 
+            CURLOPT_MAXREDIRS      => 10,       // stop after 10 redirects 
+        );
+        $ch = curl_init($url);
+        curl_setopt_array($ch, $options);
         $file_contents = curl_exec($ch);
         curl_close($ch);
-        return $file_contents;
     }
     else
     {
         $file_contents = @file_get_contents($url);
-        if (!$file_contents)
-        {
-            throw new LifeStream_FeedFetchError('Failed to open url: '.$url);
-        }
-        return $file_contents;
     }
+    if (!$file_contents)
+    {
+        throw new LifeStream_FeedFetchError('Failed to open url: '.$url);
+    }
+    return $file_contents;
 }
 
 /*
