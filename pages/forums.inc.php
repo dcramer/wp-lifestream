3b58368f93031e961a152634cc267bc09aff4cd4
David Cramer <dcramer@gmail.com = David Cramer dcramer@gmail.com>
2009-02-26 05:25:18 +0000

    Updated forum url

--- pages/forums.inc.php
+++ pages/forums.inc.php
@@ -1,5 +1,5 @@
-<h2><?php _e('LifeStream Support Forums', 'lifestream'); ?> <small>(<a href="http://www.ibegin.com/labs/forum/?CategoryID=5" target="_blank"><?php _e('Open in a new window', 'lifestream'); ?></a>)</small></h2>
+<h2><?php _e('LifeStream Support Forums', 'lifestream'); ?> <small>(<a href="http://www.ibegin.com/labs/forums/index.php#c1" target="_blank"><?php _e('Open in a new window', 'lifestream'); ?></a>)</small></h2>
 
 <p><?php _e('You may direct questions, feedback, and problems to us by posting in the forums below.', 'lifestream'); ?></p>
 
-<iframe style="width: 100%; height: 500px;" src="http://www.ibegin.com/labs/forum/?CategoryID=5"></iframe>
+<iframe style="width: 100%; height: 500px;" src="http://www.ibegin.com/labs/forums/index.php#c1"></iframe>
