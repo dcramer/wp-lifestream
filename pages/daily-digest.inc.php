0effa22b43618e10e8a8316aa0c829582b0617a2
David Cramer <dcramer@gmail.com = David Cramer dcramer@gmail.com>
2009-04-05 17:47:56 +0000

    Fixed daily digest template

--- pages/daily-digest.inc.php
+++ pages/daily-digest.inc.php
@@ -4,16 +4,13 @@ foreach ($events as $result)
 {
     $timestamp = $result->get_date();
     ?>
-    <tr class="lifestream_feedid_<?php echo $result->feed->get_constant('ID'); ?>">
-        <td class="lifestream_icon">
-            <img src="<?php echo $result->get_icon_url(); ?>" alt="<?php echo $result->feed->get_constant('ID'); ?> (feed #<?php echo $result->feed->id; ?>)" />
-        </td>
-        <td class="lifestream_hour">
-            <abbr title="<?php echo date("c", $timestamp); ?>"><?php echo date($hour_format, $timestamp); ?></abbr>
-        </td>
-        <td class="lifestream_text">
-            <?php echo $result->render($_); ?>
-        </td>
+    <tr class="lifestream_feedid_<?php echo $result->feed->id; ?> lifestream_feed_<?php echo $result->feed->get_constant('ID'); ?>">
+           <td class="lifestream_icon">
+               <a href="<?php echo htmlspecialchars($result->get_url()); ?>"><img src="<?php echo $result->feed->get_icon_url(); ?>" alt="<?php echo $result->feed->get_constant('ID'); ?> (feed #<?php echo $result->feed->id; ?>)" /></a>
+           </td>
+           <td class="lifestream_text">
+               <?php echo $result->render($_); ?>
+           </td>
     </tr>
     <?php
 }
