4f694267f38910bdacf093b331b21f6da55ac75f
David Cramer <dcramer@gmail.com = David Cramer dcramer@gmail.com>
2009-04-05 00:06:12 +0000

    Messed up version

--- lifestream.php
+++ lifestream.php
@@ -4,7 +4,7 @@ Plugin Name: LifeStream
 Plugin URI: http://www.ibegin.com/labs/wp-lifestream/
 Description: Displays your social activity in a lifestream. (Requires PHP/MySQL 5)
 Author: David Cramer
-Version: 0.95
+Version: 0.95b
 Author URI: http://www.davidcramer.net
 */
 
