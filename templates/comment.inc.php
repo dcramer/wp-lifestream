6919a0d137b9b2ef5adfae04c9522f1aa8402466
zeeg <zeeg@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-04-06 17:10:08 +0000

    Fix for tags in comments/posts
    git-svn-id: http://plugins.svn.wordpress.org/lifestream/trunk@108173 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- templates/comment.inc.php
+++ templates/comment.inc.php
@@ -10,6 +10,6 @@
 <?php } elseif ($event->data[0]['description']) { ?>
 <blockquote class="lifestream_blogpost">
 	<strong><a href="<?php echo $event->data[0]['link']; ?>"><?php echo htmlspecialchars($event->data[0]['title']); ?></a></strong><br/>
-	<?php echo htmlspecialchars($lifestream->truncate($event->data[0]['description'])); ?>
+	<?php echo htmlspecialchars(strip_tags($lifestream->truncate($event->data[0]['description']))); ?>
 </blockquote>
 <?php } ?>
