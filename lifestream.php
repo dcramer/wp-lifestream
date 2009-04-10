092a1a2ac55e8706f2b44a6fd25abaaff3aa021f
zeeg <zeeg@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-04-10 22:48:52 +0000

    Tweaked versions
    git-svn-id: http://plugins.svn.wordpress.org/lifestream/trunk@109551 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- lifestream.php
+++ lifestream.php
@@ -4,7 +4,7 @@ Plugin Name: LifeStream
 Plugin URI: http://www.ibegin.com/labs/wp-lifestream/
 Description: Displays your activity from various social networks. (Requires PHP/MySQL 5)
 Author: David Cramer
-Version: 0.982
+Version: 0.98c
 Author URI: http://www.davidcramer.net
 */
 
@@ -14,8 +14,8 @@ if (phpversion() < 5)
 	echo '<p style="font-weight: bold; font-size: 20px; padding: 10px; color: red;">LifeStream will not function under PHP 4. You need to upgrade to PHP 5 and reactivate the plugin.</p>';
 	return;
 }
-define(LIFESTREAM_VERSION, 0.982);
-define(LIFESTREAM_BUILD_VERSION, (string)LIFESTREAM_VERSION);
+define(LIFESTREAM_BUILD_VERSION, '0.98c');
+define(LIFESTREAM_VERSION, 0.98);
 //define(LIFESTREAM_PLUGIN_FILE, 'lifestream/lifestream.php');
 define(LIFESTREAM_PLUGIN_FILE, plugin_basename(__FILE__));
 define(LIFESTREAM_FEEDS_PER_PAGE, 20);
