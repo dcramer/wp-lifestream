f8127d34370ebb6e97fb8eaf0f5b33c6275474c7
David Cramer <dcramer@gmail.com = David Cramer dcramer@gmail.com>
2009-05-15 03:53:55 +0000

    Changed version compare. Add active flag to feeds (not used yet).

--- lifestream.php
+++ lifestream.php
@@ -8,8 +8,7 @@ Author: David Cramer <dcramer@gmail.com>
 Author URI: http://www.davidcramer.net
 */
 
-define(LIFESTREAM_BUILD_VERSION, '0.99.5.2');
-define(LIFESTREAM_VERSION, 0.99);
+define(LIFESTREAM_VERSION, '0.99.6');
 //define(LIFESTREAM_PLUGIN_FILE, 'lifestream/lifestream.php');
 define(LIFESTREAM_PLUGIN_FILE, plugin_basename(__FILE__));
 define(LIFESTREAM_FEEDS_PER_PAGE, 10);
@@ -1216,7 +1215,7 @@ class Lifestream
 
 		// URGENT TODO: we need to solve alters when the column already exists due to WP issues
 
-		if ($version < 0.5)
+		if (version_compare($version, 0.5, '<'))
 		{
 			// Old wp-cron built-in stuff
 			wp_clear_scheduled_hook('LifeStream_Hourly');
@@ -1225,7 +1224,7 @@ class Lifestream
 			$wpdb->query("ALTER IGNORE TABLE `".$wpdb->prefix."lifestream_event_group` ADD `version` INT(11) NOT NULL DEFAULT '0' AFTER `timestamp`, ADD `key` CHAR( 16 ) NOT NULL AFTER `version`;");
 			$wpdb->query("ALTER IGNORE TABLE `".$wpdb->prefix."lifestream_event` ADD `version` INT(11) NOT NULL DEFAULT '0' AFTER `timestamp`, ADD `key` CHAR( 16 ) NOT NULL AFTER `version`;");
 		}
-		if ($version < 0.6)
+		if (version_compare($version, 0.6, '<'))
 		{
 			$wpdb->query("ALTER IGNORE TABLE `".$wpdb->prefix."lifestream_event_group` ADD `owner` VARCHAR(128) NOT NULL AFTER `key`, ADD `owner_id` INT(11) NOT NULL AFTER `owner`;");
 			$wpdb->query("ALTER IGNORE TABLE `".$wpdb->prefix."lifestream_event` ADD `owner` VARCHAR(128) NOT NULL AFTER `key`, ADD `owner_id` INT(11) NOT NULL AFTER `owner`;");
@@ -1237,21 +1236,21 @@ class Lifestream
 			$this->safe_query($wpdb->prepare("UPDATE `".$wpdb->prefix."lifestream_event` SET `owner` = %s, `owner_id` = %d", $userdata->display_name, $userdata->ID));
 			$this->safe_query($wpdb->prepare("UPDATE `".$wpdb->prefix."lifestream_event_group` SET `owner` = %s, `owner_id` = %d", $userdata->display_name, $userdata->ID));
 		}
-		if ($version < 0.81)
+		if (version_compare($version, 0.81, '<'))
 		{
 			$wpdb->query("ALTER IGNORE TABLE `".$wpdb->prefix."lifestream_event` ADD `feed` VARCHAR(32) NOT NULL AFTER `feed_id`");
 			$this->safe_query("UPDATE IGNORE `".$wpdb->prefix."lifestream_event` as t1 set t1.`feed` = (SELECT t2.`feed` FROM `".$wpdb->prefix."lifestream_feeds` as t2 WHERE t1.`feed_id` = t2.`id`)");
 		}
-		if ($version < 0.84)
+		if (version_compare($version, 0.84, '<'))
 		{
 			$wpdb->query("ALTER IGNORE TABLE `".$wpdb->prefix."lifestream_event` ADD INDEX ( `feed` )");
 			$wpdb->query("ALTER IGNORE TABLE `".$wpdb->prefix."lifestream_event_group` ADD INDEX ( `feed` )");
 		}
-		if ($version < 0.90)
+		if (version_compare($version, 0.90, '<'))
 		{
 			$wpdb->query("ALTER IGNORE TABLE `".$wpdb->prefix."lifestream_feeds` ADD `version` int(11) default 0 NOT NULL AFTER `owner_id`");
 		}
-		if ($version < 0.96)
+		if (version_compare($version, '0.99.6', '<'))
 		{
 			$wpdb->query("ALTER IGNORE TABLE `".$wpdb->prefix."lifestream_feeds` ADD `active` tinyint(1) default 1 NOT NULL AFTER `timestamp`");
 		}
--- pages/changelog.inc.php
+++ pages/changelog.inc.php
@@ -1,6 +1,6 @@
 <h2><?php $lifestream->_e('LifeStream Change Log'); ?></h2>
 
-<p><?php $lifestream->_e('You are using <strong>wp-lifestream %s</strong>.', LIFESTREAM_BUILD_VERSION); ?></p>
+<p><?php $lifestream->_e('You are using <strong>wp-lifestream %s</strong>.', LIFESTREAM_VERSION); ?></p>
 
 <p><?php $lifestream->_e('This is the current change log, pulled directly from the latest version. It allows you to see past, and future changes.'); ?></p>
 
