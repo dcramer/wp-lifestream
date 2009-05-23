0bdc20fcfe3a6e307f51116ad6b5f5828bbc2deb
David Cramer <dcramer@gmail.com = David Cramer dcramer@gmail.com>
2009-05-23 19:24:38 +0000

    Added WordPress Codex extension.

--- feeds.inc.php
+++ feeds.inc.php
@@ -2443,4 +2443,24 @@ class LifeStream_WikipediaFeed extends LifeStream_PhotoFeed
 	}
 }
 $lifestream->register_feed('LifeStream_WikipediaFeed');
+
+class LifeStream_WordPressCodexFeed extends LifeStream_WikipediaFeed
+{
+	const ID	= 'wpcodex';
+	const NAME	= 'WordPress Codex';
+	const URL	= 'http://codex.wordpress.org/';
+	const LABEL	= 'LifeStream_ContributionLabel';
+
+
+	function get_public_url()
+	{
+		return 'http://codex.wordpress.org/index.php?title=User:'.urlencode($this->options['username']);
+	}
+
+	function get_url()
+	{
+		return 'http://codex.wordpress.org/index.php?title=Special:Contributions&feed=rss&target='.urlencode($this->options['username']);
+	}
+}
+$lifestream->register_feed('LifeStream_WordPressCodexFeed');
 ?>
