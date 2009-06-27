5c636761681196bea3a40c988eb7f74ba84ba0f4
David Cramer <dcramer@gmail.com = David Cramer dcramer@gmail.com>
2009-06-27 23:56:14 +0000

    Refactored themes to support additional information

---
+++ themes/boxy/templates/basic.inc.php
@@ -0,0 +1,7 @@
+<?php if (count($event->data) > 1) { ?>
+<ul id="<?php echo $id; ?>" class="lifestream_events"<?php if (!$visible) echo ' style="display:none;"'; ?>>
+	<?php foreach ($event->data as $chunk) { ?>
+		<li><?php echo $this->render_item($event, $chunk); ?></li>
+	<?php } ?>
+</ul>
+<?php } ?>
---
+++ themes/boxy/templates/photo.inc.php
@@ -0,0 +1,5 @@
+<div id="<?php echo $id; ?>" class="lifestream_events"<?php if (!$visible) echo ' style="display:none;"'; ?>>
+	<?php foreach ($event->data as $chunk) { ?>
+		<?php echo $this->render_item($event, $chunk); ?>
+	<?php } ?>
+</div>
