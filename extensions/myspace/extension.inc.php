3e3f11712904d0676510cffde13f2fec593d2223
zeeg <zeeg@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-01-08 23:18:55 +0000

    Fix for MySpace extending BlogFeed
    git-svn-id: http://plugins.svn.wordpress.org/lifestream/trunk@191681 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- extensions/myspace/extension.inc.php
+++ extensions/myspace/extension.inc.php
@@ -1,11 +1,52 @@
 <?php
-class Lifestream_MySpaceFeed extends Lifestream_BlogFeed
+class Lifestream_MySpaceFeed extends Lifestream_GenericFeed
 {
 	const ID			= 'myspace';
 	const NAME			= 'MySpace';
 	const URL			= 'http://www.myspace.com/';
 	const DESCRIPTION	= 'To retrieve your MySpace blog URL, visit your profile and click "View all entries" under your blog. From there, you will see an "rss" link on the top right of the page.';
+	const LABEL			= 'Lifestream_BlogLabel';
+	const HAS_EXCERPTS	= true;
+
+	function get_options()
+	{		
+		return array(
+			'url' => array($this->lifestream->__('Feed URL:'), true, '', ''),
+			'permalink_url' => array($this->lifestream->__('Website URL:'), false, '', ''),
+		);
+	}
 	
+	function _get_domain()
+	{
+		if ($this->get_option('permalink_url')) $url = $this->get_option('permalink_url');
+		else $url = $this->get_option('url');
+		preg_match('#^(http://)?([a-z0-9\-\.]*\.)?([a-z0-9\-]+(?:\.[a-z0-9\-]+)?)/?#i', $url, $matches);
+		return $matches[3];
+	}
+	
+	function get_public_name()
+	{
+		if ($this->get_option('feed_label'))
+		{
+			return $this->get_option('feed_label');
+		}
+		return $this->_get_domain();
+	}
+	
+	function get_public_url()
+	{
+		if ($this->get_option('permalink_url')) return $this->get_option('permalink_url');
+		
+		return 'http://'.$this->_get_domain();
+	}
+
+	function yield($row, $url, $key)
+	{
+		$data = parent::yield($row, $url, $key);
+		$author =& $row->get_item_tags(SIMPLEPIE_NAMESPACE_DC_11, 'creator');
+		$data['author'] = $this->lifestream->html_entity_decode($author[0]['data']);
+		return $data;
+	}	
 }
 $lifestream->register_feed('Lifestream_MySpaceFeed');
 ?>
