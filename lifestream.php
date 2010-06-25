8104c4aee5dc4a08f01c9b005980e4539e182115
zeeg <zeeg@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-06-25 17:37:59 +0000

    Fix duplicate constants
    git-svn-id: http://plugins.svn.wordpress.org/lifestream/trunk@257293 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- lifestream.php
+++ lifestream.php
@@ -18,14 +18,6 @@ if (!function_exists('plugin_basename'))
 	function add_action() {}
 }
 
-define('LIFESTREAM_VERSION', '0.99.9.7.1');
-define('LIFESTREAM_PLUGIN_FILE', plugin_basename(__FILE__));
-define('LIFESTREAM_PATH', dirname(__FILE__));
-define('LIFESTREAM_URL', plugins_url($path = '/'.LIFESTREAM_PATH));
-define('LIFESTREAM_FEEDS_PER_PAGE', 10);
-define('LIFESTREAM_EVENTS_PER_PAGE', 25);
-define('LIFESTREAM_ERRORS_PER_PAGE', 25);
-
 if (version_compare(PHP_VERSION, '5.0', '<'))
 {
 	if ($_GET['activate'] == true)
